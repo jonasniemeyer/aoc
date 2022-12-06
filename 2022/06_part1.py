894e9b6756b7422f371915e98d36f8be074988dd
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-12-06 11:42:43 +0100

    day 6

---
+++ 2022/06_part1.py
@@ -0,0 +1,19 @@
+def detect_start_position(input_str: str, size: int) -> int:
+    marker = [0]*size
+    
+    for index, letter in enumerate(input_str):
+        marker.pop(0)
+        marker.append(letter)
+        # fill first values
+        if index < (size-1):
+            continue
+
+        if len(marker) == len(set(marker)):
+            return index+1
+    
+    return -1
+
+if __name__ == "__main__":
+    with open("input6.txt", "r") as file:
+        puzzle_input = file.read()
+    print(detect_start_position(puzzle_input, 4))
