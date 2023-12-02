98bef5838bdd77821fdda8c68c988b07bda4e71c
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-12-02 23:47:46 +0100

    add part2

--- 2023/01_1.c
+++ 2023/01_1.c
@@ -4,21 +4,26 @@
 #include <string.h>
 
 uint64_t part1(FILE *file);
+uint64_t part2(FILE *file);
+
+#define MAX_LINE    1000
 
 int main(int argc, char *argv[])
 {
     FILE *file = fopen(argv[1], "r");
-    printf("%llu", part1(file));
+    printf("%llu\n", part1(file));
+    rewind(file);
+    printf("%llu\n", part2(file));
 }
 
 uint64_t part1(FILE *file)
 {
-    char line[1000];
+    char line[MAX_LINE];
     uint64_t sum = 0;
     uint8_t first;
     uint8_t last = 0;
 
-    while (fgets(line, 1000, file)) {
+    while (fgets(line, MAX_LINE, file)) {
         first = 1;
         for (size_t index = 0; index < strlen(line); index++) {
             if (isdigit(line[index])) {
@@ -28,6 +33,57 @@ uint64_t part1(FILE *file)
                     first = 0;
                 } else {
                     last = (line[index]-'0');
+                }
+            }
+        }
+        sum += last;
+    }
+    
+    return sum;
+}
+
+uint64_t part2(FILE *file)
+{
+    char line[MAX_LINE];
+    uint64_t sum = 0;
+    uint8_t first;
+    uint8_t last = 0;
+
+    char *numbers[] = {
+        "placeholder",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    };
+
+    while (fgets(line, MAX_LINE, file)) {
+        first = 1;
+        for (size_t index = 0; index < strlen(line); index++) {
+            if (isdigit(line[index])) {
+                if (first) {
+                    sum += (line[index]-'0') * 10;
+                    last = (line[index]-'0');
+                    first = 0;
+                } else {
+                    last = (line[index]-'0');
+                }
+            } else {
+                for (size_t i = 1; i < 10; i++) {
+                    if (strncmp(&line[index], numbers[i], strlen(numbers[i])) == 0) {
+                        if (first) {
+                            sum += i * 10;
+                            last = i;
+                            first = 0;
+                        } else {
+                            last = i;
+                        }
+                    }
                 }
             }
         }
