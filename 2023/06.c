a0c3d7513cf91b7cd30086cb4e3b6b938e5de943
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-12-06 17:23:44 +0100

    add part 1

---
+++ 2023/06.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+uint64_t part1(FILE *file);
+uint64_t parse_line(FILE *file, uint64_t array[]);
+uint64_t compute_possible_charges(uint64_t time, uint64_t record);
+
+#define MAX_LINE    1000
+#define MAX_RACES   100
+
+int main()
+{
+    FILE *file = fopen("input06.txt", "r");
+    printf("Part 1: %llu\n", part1(file));
+}
+
+uint64_t part1(FILE *file)
+{
+    uint64_t time[MAX_RACES];
+    uint64_t record[MAX_RACES];
+    uint64_t margin_error = 1;
+
+    uint64_t total_races = parse_line(file, time);
+    parse_line(file, record);
+    printf("%llu %llu\n", time[2], record[3]);
+
+    for (size_t i = 0; i < total_races; i++) {
+        margin_error *= compute_possible_charges(time[i], record[i]);
+    }
+
+    return margin_error;
+}
+
+uint64_t parse_line(FILE *file, uint64_t array[])
+{
+    #define IN  1
+    #define OUT 0
+
+    char line[MAX_LINE];
+    size_t index = 0;
+    uint8_t state = OUT;
+    uint64_t number = 0;
+
+    fgets(line, MAX_LINE, file);
+
+    for (size_t i = 0; i < strlen(line); i++) {
+        if (isdigit(line[i])) {
+            state = IN;
+            number *= 10;
+            number += (line[i] - '0');
+            if ((i == (strlen(line)-1))) {
+                array[index++] = number;
+            }
+        } else if (state == IN) {
+            state = OUT;
+            array[index++] = number;
+            number = 0;
+        }
+    }
+
+    return index;
+}
+
+uint64_t compute_possible_charges(uint64_t time, uint64_t record)
+{
+    uint64_t total = 0;
+    uint64_t distance;
+
+    for (size_t charge = 1; charge < time; charge++) {
+        if (((charge) * (time-charge)) > record) total++;
+    }
+
+    return total;
+}
