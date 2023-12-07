da4527dcad9d636428647034d897a39c2b1fc96d
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-12-07 19:43:30 +0100

    add part 2

--- 2023/07.c
+++ 2023/07.c
@@ -7,6 +7,7 @@
 uint64_t compute_winnings(FILE *file, uint64_t (*hand_parser)(char *line));
 uint64_t parse_lines(FILE *file, uint64_t hands[], uint64_t bids[], uint64_t (*hand_parser)(char *line));
 uint64_t parse_hand(char *line);
+uint64_t parse_hand_p2(char *line);
 uint64_t parse_bid(char *line);
 void dual_sort(uint64_t *hands, uint64_t *bids, uint64_t total_elements);
 
@@ -18,6 +19,8 @@ int main()
     FILE *file = fopen("input07.txt", "r");
     printf("Part 1: %llu\n", compute_winnings(file, parse_hand));
     rewind(file);
+    printf("Part 1: %llu\n", compute_winnings(file, parse_hand_p2));
+    fclose(file);
 }
 
 uint64_t compute_winnings(FILE *file, uint64_t (*hand_parser)(char *line))
@@ -85,7 +88,79 @@ uint64_t parse_hand(char *line)
         bit_offset -= 12;
     }
 
-    uint64_t checkvalue = 0, rank = 99;
+    uint64_t checkvalue = 0, rank;
+    for (size_t i = 0; i < 13; i++) {
+        if (counts[i] != 0) {
+            checkvalue += (counts[i]*counts[i]);
+        }
+    }
+
+    switch (checkvalue) {
+        case 5:
+            rank = 1;
+            break;
+        case 7:
+            rank = 2;
+            break;
+        case 9:
+            rank = 3;
+            break;
+        case 11:
+            rank = 4;
+            break;
+        case 13:
+            rank = 5;
+            break;
+        case 17:
+            rank = 6;
+            break;
+        case 25:
+            rank = 7;
+            break;
+    }
+
+    card = card | (rank << 60);
+    return card;
+}
+
+uint64_t parse_hand_p2(char *line)
+{
+    uint64_t card = 0, bit_offset = 48;
+    char cards[] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'};
+    uint64_t counts[13] = {0};
+    uint64_t jacks = 0;
+
+    for (size_t i = 0; i < 5; i++) {
+        if (line[i] == 'J') {
+            jacks++;
+            bit_offset -= 12;
+            continue;
+        }
+
+        uint64_t index = 0;
+        for (size_t j = 0; j < 12; j++) {
+            if (line[i] == cards[j]) {
+                counts[j+1]++;
+                break;
+            }
+            index++;
+        }
+
+        card = card | ((uint64_t)1 << (bit_offset+index));
+        bit_offset -= 12;
+    }
+
+    // increment the maximum card count by the number of jokers
+    uint64_t max_count_idx = 0, max_count = 0;
+    for (size_t i = 0; i < 13; i++) {
+        if (counts[i] > max_count) {
+            max_count = counts[i];
+            max_count_idx = i;
+        }
+    }
+    counts[max_count_idx] += jacks;
+
+    uint64_t checkvalue = 0, rank;
     for (size_t i = 0; i < 13; i++) {
         if (counts[i] != 0) {
             checkvalue += (counts[i]*counts[i]);
